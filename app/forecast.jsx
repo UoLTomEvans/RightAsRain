b8108bc989e3b65234294efa334367f7233408e7
Tom Evans <tommarkevans@gmail.com>
2024-10-15 17:08:15 +0100

    App setup

---
+++ app/forecast.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const forecast = () => {
+  return (
+    <View>
+      <Text>forecast</Text>
+    </View>
+  );
+};
+
+export default forecast;
