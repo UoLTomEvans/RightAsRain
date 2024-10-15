6113b121693170af416621ef225a80036d14bc30
Tom Evans <tommarkevans@gmail.com>
2024-10-15 19:14:21 +0100

    Tabs

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,30 @@
-import { SplashScreen, Stack } from "expo-router";
+import { SplashScreen, Tabs } from "expo-router";
 import { useFonts } from "expo-font";
 import { useEffect } from "react";
+import { View, Text, Image } from "react-native";
+import { icons } from "../constants";
 
 SplashScreen.preventAutoHideAsync();
+
+// Component for TabIcons in layout
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
 
 const RootLayout = () => {
   // Bring in all fonts
@@ -26,22 +48,51 @@ const RootLayout = () => {
 
   if (!fontsLoaded && !error) return null;
 
-  // Then load screens
   return (
-    <Stack>
-      <Stack.Screen
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#FFA001",
+        tabBarInactiveTintColor: "#CDCDE0",
+        tabBarStyle: {
+          backgroundColor: "#161622",
+          borderTopWidth: 1,
+          borderTopColor: "#232533",
+          height: 84,
+        },
+      }}
+    >
+      <Tabs.Screen
         name="index"
         options={{
+          title: "Home",
           headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.home}
+              color={color}
+              name="Home"
+              focused={focused}
+            />
+          ),
         }}
       />
-      <Stack.Screen
+      <Tabs.Screen
         name="forecast"
         options={{
+          title: "Forecast",
           headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.forecast}
+              color={color}
+              name="Forecast"
+              focused={focused}
+            />
+          ),
         }}
       />
-    </Stack>
+    </Tabs>
   );
 };
 
---
+++ constants/icons.js
@@ -0,0 +1,9 @@
+import home from "../assets/icons/home.png";
+import search from "../assets/icons/search.png";
+import forecast from "../assets/icons/forecast.png";
+
+export default {
+  home,
+  search,
+  forecast,
+};
